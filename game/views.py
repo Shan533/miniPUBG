25210718b8d053f6dea4e614122b7bcd48c8cdb9
Shanshan Wu <790219068@qq.com>
2023-08-14 02:40:01 +0800

    create start page

--- game/views.py
+++ game/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    line1 = '<h1 style="text-align: center">Hello, Aaron</h1>'
+    line2 = '<img src= "https://labuget.ro/wp-content/uploads/2018/06/pubg.jpg">'
+    line3 = '<hr>'
+    line4 = '<a href="play/">START GAME</a>'
+    return HttpResponse(line1 + line4 + line3 + line2)
+
+def play(request):
+    line1 = '<h1 style = "text-align: center">YOU DIE!</h1>'
+    line2 = '<img src= "https://img4.18183.com/uploads/allimg/180111/88-1P111151448-50.jpg@q_80">'
+    return HttpResponse(line1 + line2)
